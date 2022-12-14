9c91f7ab153d7104854d909c317f4b06155d8b7c
root <root@KENZA.localdomain>
2022-12-14 13:23:38 +0200

    Added 2-binary_tree_insert_right.c

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * If parent already has a right-child, the new node must take its place,
+ * and the old right-child must be set as the right-child of the new node.
+ * @parent: pointer to the node to insert the right-child in
+ * @value: the value to store in the new node
+ * Return: pointer to the created node or NULL on failure
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node, *temp;
+	
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	temp = parent->right;
+
+	if (temp != NULL)
+	{
+		parent->right = new_node;
+		new_node->right = temp;
+		temp->parent = new_node;
+	}
+	else
+	{
+		new_node->right = NULL;
+	}
+	return (new_node);
+}
